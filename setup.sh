040cfe45ffc4d0373a0b698a64bf7408b54f6292
alchemists water <alchemistswater@fastmail.com>
2020-02-05 17:23:30 -0500

    adding extras

--- setup.sh
+++ setup.sh
@@ -3,12 +3,23 @@
 # Export the path to this directory for later use in the script
 export LINKDOT=$PWD
 
-sudo pacman -S  go nextcloud-client gvim scrot htop arc-gtk-theme firefox sxhkd zathura-pdf-mupdf \
-		xclip gnome-keyring xfce4-notifyd xsel xdotool xorg-server xorg-xinit xorg-xrdb \ 
-		xorg-xprop xdo pulseaudio-alsa exa pavucontrol tmux bash-completion pamixer \
-		pulseaudio fzf cmus bspwm feh git openssh slock ttf-roboto ttf-roboto-mono dash
+sudo pacman -S  go ttf-joypixels ttf-croscore noto-fonts-cjk noto-fonts \
+	nextcloud-client gvim scrot htop arc-gtk-theme firefox sxhkd \
+	zathura-pdf-mupdf xclip gnome-keyring xfce4-notifyd xsel xdotool \
+	xorg-server xorg-xinit xorg-xrdb xorg-xprop xdo pulseaudio-alsa \
+	exa pavucontrol tmux bash-completion pamixer pulseaudio fzf cmus \
+	bspwm feh git openssh slock ttf-roboto ttf-roboto-mono dash
 
 sudo ln -sfT dash /usr/bin/sh
+sudo ln -sf /etc/fonts/conf.avail/75-joypixels.conf /etc/fonts/conf.d/
+sudo ln -sf /etc/fonts/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
+sudo ln -sf /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d/
+sudo ln -sf /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d/
+
+sudo install -Dm 644 other/freetype2.sh /etc/profile.d/
+sudo install -Dm 644 other/local.conf /etc/fonts/
+sudo install -Dm 644 other/dashbinsh.hook /usr/share/libalpm/hooks/
+sudo install -Dm 644 other/50-mouse-acceleration.conf /etc/X11/xorg.conf.d/
 
 mkdir -p ~/.config ~/.builds /storage/Music /storage/Images /storage/Downloads /storage/steamlibrary/
 
@@ -32,16 +43,15 @@ cd ~/.builds/aw_dmenu
 make
 sudo make install
 
-yay -S  picom pfetch-git moka-icon-theme lxappearance sox imagemagick i3lock \
-	profile-sync-daemon ttf-font-awesome lemonbar-xft-git st-luke-git
+yay -S  picom pfetch-git moka-icon-theme lxappearance sox imagemagick \
+	i3lock ttf-font-awesome lemonbar-xft-git st-luke-git
 
 cd ~
 
 read -p "-- Install gaming goodness? May take a minute. [y/N] " yna
 case $yna in
             [Yy]* ) yay -S steam steam-native-runtime lib32-libpulse \
-		    lib32-alsa-plugins \
-                    lutris lutris-wine-meta itch
+	lib32-alsa-plugins lutris lutris-wine-meta itch
 		    mv $HOME/.steam/steam/steam /storage/steamlibrary/
 		    mv $HOME/.steam/steam/steamapps /storage/steamlibrary/
 		    ln -sf /storage/steamlibrary/* $HOME/.steam/steam/
@@ -64,8 +74,6 @@ ln -sf $LINKDOT/config/* /home/$USER/.config/
 
 cd /usr/bin
 sudo ln -sf $LINKDOT/scripts/* /usr/bin/
-
-systemctl --user enable psd
 
 cd ~
 
